4b2748effa43c64cba28d35b431cd1afc3c5df40
govardhanreddy111 <>
2020-06-28 20:18:11 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            users : []
+        }
+    }
+
+
+    componentDidMount() {
+        fetch('https://api.github.com/users')
+            .then(res => res.json())
+            .then(result => {
+                this.setState({ users : result})
+            })
+    }
+
+
+    render(){
+        return(
+            <div>
+                {this.state.users.map(user => {
+                   return  (<div style={{ display: 'flex', justifyContent: 'flex-start', alignItems: 'center', padding: '20px' }}>
+                                    <img src={user.avatar_url} width={100} />
+                                    <div style={{ padding : '20px'}}>{user.login}</div>
+                                </div>
+                   )
+                })}
+            </div>
+        )
+    }
 }
 
 export default App;
